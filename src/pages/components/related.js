87f4e273842e2330cadaf61b9f14133e30ec29b3
Gerardo <gerardolopezduenas@gmail.com>
2017-12-26 15:30:44 -0600

    Added HomeLayout and Related components.

---
+++ src/pages/components/related.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './related.css';
+import logo from '../../../images/logo.png';
+
+function Related() {
+  return (
+    <div className="Related">
+      <img src={logo} alt="" width={250} />
+    </div>
+  );
+}
+
+export default Related;
